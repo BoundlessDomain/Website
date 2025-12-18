305f845e94e386c93d868cf056ba241f7f00556b
Bobby Yu <Work.BobbyYu@gmail.com>
2025-12-18 16:48:26 +0100

    V2.23.1: 1. Code optimisation 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+    modularizeImports: {
+        'lucide-react': {
+            transform: 'lucide-react/dist/esm/icons/{{lowerCase member}}',
+        },
+    },
 };
 
 export default nextConfig;
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,10 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log("Supabase Config:", {
-    url: supabaseUrl,
-    keyExists: !!supabaseKey,
-    keyLength: supabaseKey?.length
-});
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
